e51ab4947bead7cb90c0787e2163d9fb6ef9e81f
Shariar Islam <shariarn85@gmail.com>
2024-01-08 14:13:15 +0600

    built in data type

---
+++ built-in-data-type/index.js
@@ -0,0 +1,18 @@
+// built in data type
+// number, string, boolean, void, undefined, null
+var userId;
+var firstName;
+var lastName;
+var isActivated;
+userId = 101;
+firstName = "Shariar";
+lastName = "Islam";
+isActivated = true;
+console.log("userId: ".concat(userId, ", User Name: ").concat(firstName, " ").concat(lastName, ", Account Activate: ").concat(isActivated));
+// console.log(firstName);
+// console.log(lastName);
+// console.log(isActivated);
+function display() {
+    console.log('Hi i am display');
+}
+display();
---
+++ built-in-data-type/index.ts
@@ -0,0 +1,25 @@
+// built in data type
+// number, string, boolean, void, undefined, null
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let isActivated: boolean;
+
+userId = 101;
+firstName = "Shariar";
+lastName = "Islam";
+isActivated = true;
+
+
+console.log(`userId: ${userId}, User Name: ${firstName} ${lastName}, Account Activate: ${isActivated}`);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(isActivated);
+
+
+function display(): void {
+    console.log('Hi i am display');
+}
+
+display()
