a5963965d09d4ac7269039705277c6eff7d035c9
Shariar Islam <shariarn85@gmail.com>
2024-01-08 14:57:29 +0600

    Array Data Type

---
+++ user-define-data-type/Array-Data-Type.js
@@ -0,0 +1,9 @@
+var userName;
+var userProfession;
+var multipleTypeArray;
+userName = ["shariar", "islam", "niaj"];
+userProfession = ["Developer", "Blogger", "Web Designer"];
+multipleTypeArray = ["fsafsda", 5, 7];
+console.log(userName);
+console.log(userProfession);
+console.log(multipleTypeArray);
---
+++ user-define-data-type/Array-Data-Type.ts
@@ -0,0 +1,11 @@
+let userName: string[];
+let userProfession: Array<string>;
+let multipleTypeArray: (string | number)[];
+
+userName = ["shariar", "islam", "niaj"];
+userProfession = ["Developer", "Blogger", "Web Designer"];
+multipleTypeArray = ["fsafsda", 5, 7];
+
+console.log(userName);
+console.log(userProfession);
+console.log(multipleTypeArray);
