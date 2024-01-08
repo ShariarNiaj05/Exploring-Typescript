e08754d719ccbed5fd0cb0ec50f24744240400cc
Shariar Islam <shariarn85@gmail.com>
2024-01-08 15:37:48 +0600

    Tuple Data Type

---
+++ user-define-data-type/Tuple-Data-Type.js
@@ -0,0 +1,6 @@
+// mixed data - key, value 
+var user;
+user = [101, 'Niaj'];
+console.log(user);
+user.push(102, 'Shariar');
+console.log(user);
---
+++ user-define-data-type/Tuple-Data-Type.ts
@@ -0,0 +1,10 @@
+// mixed data - key, value 
+
+let user: [number, string]
+
+user = [101, 'Niaj']
+
+console.log(user);
+
+user.push(102, 'Shariar')
+console.log(user);
